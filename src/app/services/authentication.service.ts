3ad44c168a5ae027c9bc3bc67d30335cf001eb8a
Enrique Díaz Romero <enriqued_93@hotmail.com>
2020-01-07 23:39:25 -0600

    Add AuthenticationService

---
+++ src/app/services/authentication.service.ts
@@ -0,0 +1,28 @@
+import {Injectable} from '@angular/core';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class AuthenticationService {
+
+    private user: Backendless.User = null;
+
+    public isUserAuthenticated(): Promise<boolean> {
+        return Backendless.UserService.isValidLogin();
+    }
+
+    public logIn(email: string, password: string): Promise<boolean> {
+        return Backendless.UserService.login(email, password, true)
+            .then((loggedInUser) => {
+                this.user = loggedInUser;
+                return true;
+            })
+            .catch((error) => {
+                return false;
+            });
+    }
+
+    public logOut(): Promise<void> {
+        return Backendless.UserService.logout();
+    }
+}
