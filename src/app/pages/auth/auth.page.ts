db34f4ffa26535f8b7b0d81e31e145aed3a573ac
ShibaK7 <shirou.k7@gmail.com>
2020-01-10 15:03:11 -0600

    crear menu del auth page del proyecto

--- src/app/pages/auth/auth.page.ts
+++ src/app/pages/auth/auth.page.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {AuthenticationService} from '../../services/authentication.service';
 import {Router} from '@angular/router';
+import { MenuController } from '@ionic/angular';
 
 @Component({
     selector: 'app-auth',
@@ -9,16 +10,41 @@ import {Router} from '@angular/router';
 })
 export class AuthPage implements OnInit {
 
+    public user: Backendless.User;
+
     constructor(private router: Router,
-                private authenticationService: AuthenticationService) {
+                private authenticationService: AuthenticationService,
+                private menu: MenuController) {
     }
 
     ngOnInit() {
+        this.authenticationService.getCurrentUser().then((user:Backendless.User) => {
+            this.user = user;
+            console.log(this.user.username);
+        });
     }
 
     logOut() {
         this.authenticationService.logOut().then(() => {
             return this.router.navigate(['public']);
         });
+    }
+
+    openFirst() {
+        this.menu.enable(true, 'first');
+        this.menu.open('first');
+    }
+
+    openEnd() {
+        this.menu.open('end');
+    }
+
+    openCustom() {
+        this.menu.enable(true, 'custom');
+        this.menu.open('custom');
+    }
+
+    toggleMenu() {
+        this.menu.toggle();
     }
 }
